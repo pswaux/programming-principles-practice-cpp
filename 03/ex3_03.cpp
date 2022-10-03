5136a2c31f8760cd1bf037e06a2b7731066ff779
pswaux <kutay.akgun@gmail.com>
2022-10-04 00:57:08 +0300

    ex3_03.cpp added

---
+++ 03/ex3_03.cpp
@@ -0,0 +1,19 @@
+/**
+ * Exercise 3_3
+ *
+ * Write a program that doesn’t do anything, but declares a number of variables 
+ * with legal and illegal names (such as int double = 0;), so that you can see 
+ * how the compiler reacts.
+ *
+ */
+#include "std_lib_facilities.h"
+
+int main() 
+{
+   //int string = 5;
+   //double int = 0;
+   //char ch = 'a';
+   //bool is_correct = false;
+   //int _number = 10;
+   //double floats&doubles = 0.0;
+}
