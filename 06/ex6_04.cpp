38e29df26aaac52579589753a09f450a200e83a7
Kutay Akgun <kutay.akgun@gmail.com>
2022-12-13 01:21:40 +0300

    a little typo in the comment has been fixed

--- 06/ex6_04.cpp
+++ 06/ex6_04.cpp
@@ -1,5 +1,5 @@
 /**
- * Exercise 4_4
+ * Exercise 6_4
  *
  * Define a class Name_value that holds a string and a value. Rework 
  * exercise 19 in Chapter 4 to use a vector<Name_value> instead of two vectors.
