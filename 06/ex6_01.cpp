668e3a812a5ae26481d5618f3f7a251f79eec8e3
pswaux <kutay.akgun@gmail.com>
2022-12-13 01:01:45 +0300

    ex6_01.cpp added

---
+++ 06/ex6_01.cpp
@@ -0,0 +1,14 @@
+/**
+ * Exercise 6_1
+ *
+ * If you haven’t done so already, 
+ * do the Try this exercises from this chapter.
+ *
+ */
+#include <iostream>
+
+int main()
+{
+   std::cout << "If you haven't done so already,\n"
+      << "do the Try this exercises from this chapter\n";
+}
